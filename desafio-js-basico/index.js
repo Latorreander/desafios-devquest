578113fe99e8d837343b34d1e83d8c1b659b07ee
Latorreander <latorreander@gmail.com>
2022-05-13 14:18:18 -0300

    desafio js básico

---
+++ desafio-js-basico/index.js
@@ -0,0 +1,45 @@
+//---SOMA---
+
+function somar(numero1, numero2) {
+  let Soma = numero1 + numero2;
+  return `A Soma dos dois números é: ${Soma}`;
+}
+
+somar(3, 5);
+let resultado1 = somar(3, 5);
+console.log(resultado1);
+//-------------------------------------------------
+
+//---SUBTRAÇÃO---
+
+function subtrair(numero1, numero2) {
+  let Subtracao = numero1 - numero2;
+  return `A Subtração dos dois números é: ${Subtracao}`;
+}
+
+subtrair(30, 15);
+let resultado2 = subtrair(30, 15);
+console.log(resultado2);
+//------------------------------------------------
+
+//---DIVISÃO---
+
+function dividir(numero1, numero2) {
+  let Divisao = numero1 / numero2;
+  return `A Divisão dos dois números é: ${Divisao}`;
+}
+
+dividir(50, 2);
+let resultado3 = dividir(50, 2);
+console.log(resultado3);
+
+//---MULTIPLICAÇÃO---
+
+function multiplicar(numero1, numero2) {
+  let multiplicacao = numero1 * numero2;
+  return `A Multiplicação dos dois números é: ${multiplicacao}`;
+}
+
+multiplicar(3, 3);
+const resultado4 = multiplicar(3, 3);
+console.log(resultado4);
