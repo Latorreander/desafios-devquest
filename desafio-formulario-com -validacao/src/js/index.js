3a295dd813e140bd578c0c0d67df3159f8774783
Latorreander <latorreander@gmail.com>
2022-07-14 13:03:12 -0300

    formulario com validação no js

--- desafio-formulario-com -validacao/src/js/index.js
+++ desafio-formulario-com -validacao/src/js/index.js
@@ -1,9 +1,9 @@
-function validar(){
-    const campoFormulario = document.querySelectorAll("input")
-    campoFormulario.forEach(function(input){
-        if(input.value ==""){
-            input.nextElementSibling.classList.add("ativo")
-            input.classList.add("nao-preenchido")
-        }else(input.classList.add("preenchido"))   
-    }) 
+function validar() {
+  const campoFormulario = document.querySelectorAll("input");
+  campoFormulario.forEach(function (input) {
+    if (input.value == "") {
+      input.nextElementSibling.classList.add("ativo");
+      input.classList.add("nao-preenchido");
+    } else input.classList.add("preenchido");
+  });
 }
