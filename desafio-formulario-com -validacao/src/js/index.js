d074b68c0937d0ecf43775643bac1785a0f484aa
Latorreander <latorreander@gmail.com>
2022-07-21 12:13:31 -0300

    trocado de "onclick" para "eventlistner"

--- desafio-formulario-com -validacao/src/js/index.js
+++ desafio-formulario-com -validacao/src/js/index.js
@@ -1,9 +1,13 @@
-function validar() {
+
   const campoFormulario = document.querySelectorAll(".area");
-  campoFormulario.forEach(function (area) {
-      if (area.value == "") {
-        area.nextElementSibling.classList.add("ativo")
-        area.classList.add("nao-preenchido") 
-      } else area.classList.add("preenchido")
-  });
-}
+  const botao = document. getElementById("botao")
+  
+  botao.addEventListener("click", function(){
+    campoFormulario.forEach(function (area) {
+        if (area.value == "") {
+          area.nextElementSibling.classList.add("ativo")
+          area.classList.add("nao-preenchido") 
+        } else area.classList.add("preenchido") 
+    });
+  })
+
