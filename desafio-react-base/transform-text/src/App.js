8c660ae41b5be6889ff7bf4b4b0fd68abb569589
Latorreander <latorreander@gmail.com>
2022-12-08 21:11:23 -0300

    "desafio react base"

---
+++ desafio-react-base/transform-text/src/App.js
@@ -0,0 +1,15 @@
+
+import Text from './components/texttransform/textelement'
+import './App.css';
+
+function App() {
+    return (
+      <>
+      <Text paragraph='esse é um parágrafo maiúsculo e colorido do componente Text'/>
+      </>
+    )
+    
+    
+}
+
+export default App;
---
+++ desafio-react-base/transform-text/src/components/texttransform/textelement.js
@@ -0,0 +1,10 @@
+
+const Text = ({paragraph}) =>{
+    return (
+       <div>
+        <p style={{color:'green'}}>{paragraph.toUpperCase()}</p>
+       </div> 
+    )
+}
+
+export default Text
